76ad3c415d6309b85c6d15eb85c24778eeca8eab
Isaac Lim <r1522660@mvrht.net>
2019-03-22 14:09:08 +0800

    rootin' tootin' dialogue

--- combat.py
+++ combat.py
@@ -7,8 +7,10 @@ def print_health(player):
     display = int(player.current_health/player.max_health * 20)
     send_text(player.name.upper() + "  [%-20s] " % ('='*display) + str(player.current_health) + "/" + str(player.max_health))
 
-shooting_text = ['Bam! * has shot & dead center', '* shoots right into &\'s soul', 'You can hear the sound of *\'s bullet piercing through &\'s skin']
+shooting_text = ['Bam! You just shot & dead center', 'You shoots right into one of &\'s heart', 'You can hear the sound of a tight bullet piercing through &\'s skin']
 missing_text = ['Ooh- the bullet just barely scrapes a knee but bounces past', 'You can hear the sound of a bullet just barely missing', 'The bullet goes completely haywire and completely misses']
+alien_text = ['The slippin\', squishin\', sound of tentacles reaches deep into ya soul', 'You can see a slow pricklin\' as fluorescent slime covers your body']
+alien_miss_text = ['The gloop scrapes the corner of your hat', 'The tentacle just barely misses you']
 
 class Enemy():
     def __init__(self, name, max_health, ac):
@@ -20,9 +22,9 @@ class Enemy():
         self.dead = False
 
     def get_hit(self, dmg, hit, enemyName):
-        global shooting_text
+        global shooting_text, missing_text
         if hit >= self.ac:
-            send_text(choice(shooting_text).replace('&', self.name).replace('*', enemyName))
+            send_text(choice(shooting_text).replace('&', self.name))
             self.current_health -= dmg
             if self.current_health <= 0:
                 self.death()
@@ -46,6 +48,16 @@ class Player(Enemy):
     def death(self):
         send_text(self.name + " is dead")
         exit("That's a game over")
+
+    def get_hit(self, dmg, hit, enemyName):
+        global alien_text, alien_miss_text
+        if hit >= self.ac:
+            send_text(choice(alien_text).replace('&', self.name))
+            self.current_health -= dmg
+            if self.current_health <= 0:
+                self.death()
+        else:
+            send_text(choice(alien_miss_text))
 
     def consume(self, item):
         """
--- gameloop.py
+++ gameloop.py
@@ -19,28 +19,32 @@ def get_input(prompt):
     return input(prompt)
 
 if __name__ == '__main__':
-    player = Player('Sheriff Player', 24, 11)
-    enemy = Enemy("Rootin' Tootin' Putin", 24, 11)
+    send_text("Welcome to Far Space, \nthe best game in the western regions of space, except space has no west because space doesn't have poles as it is not a planet, but we can mostly ignore this because we have defined it as un-dandy, pardner\nAll un-dandy information will be reported to the council of the dandiness, lead by supreme leader Dan\nIf you dare disobey Dan, you will be shot with a shotgun with a cowboy hat on it\n\nSupereme leader Dan has come down with a case of the backdoor trots so he's ordered y'all to invade this alien spaceship. Have a hog-killin' time! Yee-haw!")
     while True:
-        input_valid = False
-        while not input_valid:
-            try:
-                move_choice = int(get_input("Press 1 to Attack and 2 to not Attack"))
-                if move_choice == 1 or move_choice == 2:
-                    input_valid = True
-                else:
+        player = Player('Player', 24, 11)
+        current_enemy = Enemy("Rootin' Tootin' Putin", 24, 11)
+
+        in_battle = True
+        while in_battle:
+            input_valid = False
+            while not input_valid:
+                try:
+                    move_choice = int(get_input("Press 1 to shoot the darn alien and 2 to not shoot the darn alien. Dan recommends y'all shoot the darn alien"))
+                    if move_choice == 1 or move_choice == 2:
+                        input_valid = True
+                    else:
+                        send_text("Invalid input")
+                except ValueError:
                     send_text("Invalid input")
-            except ValueError:
-                print("Invalid input")
-        send_text("")
-        if move_choice == 1:
-            player.physical_attack(enemy)
-
-        enemy.physical_attack(player)
-
-        if enemy.dead:
             send_text("")
-            exit()
+            if move_choice == 1:
+                player.physical_attack(current_enemy)
 
-        print_health(player)
-        print_health(enemy)
+            current_enemy.physical_attack(player)
+
+            if current_enemy.dead:
+                # move to next room
+                in_battle = False
+
+            print_health(player)
+            print_health(current_enemy)
